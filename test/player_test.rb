6448acc9ffd2664761b5535a44c60252d08d6fd7
Andrew Smith <andrewpatrick138@gmail.com>
2021-02-08 16:49:04 -0800

    add scaffolding

---
+++ test/player_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/_'
+class PlayerTest < Minitest::Test
+  def test_it_exists
+  end
+  def test_it_has_attributes
+  end
+  def test_it_can_have_different_attributes
+  end
+end
