27d97b761de041f5a44d70c2154e0ecf844ea6ef
jaharaclark <jaharamclark@gmail.com>
2021-02-11 12:25:21 -1000

    Add functionality to turn test/implementation

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,19 @@
 class Turn
-  attr_reader :board_comp, :board_user
+  attr_reader :secret_code,
+              :user_code,
+              :exact_counter, 
+              :near_counter
+  attr_accessor :has_won
   
-  def initialize(board_comp, board_user)
-    @board_comp = board_comp
-    @board_user = board_user
+  def initialize(secret_code, user_code)
+    @secret_code = secret_code
+    @user_code = user_code
+    @exact_counter = 0
+    @near_counter = 0
+    @has_won = false
+  end
+
+  def has_won?
+    @has_won
   end
 end
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -12,9 +12,9 @@ class TurnTest < Minitest::Test
     globe_4 = Globe.new('y')
     globes_A = [globe_1, globe_2, globe_3, globe_4]
     globes_B = [globe_4, globe_3, globe_2, globe_1]
-    board_comp = Board.new(globes_A)
-    board_user = Board.new(globes_B)
-    turn = Turn.new(board_comp, board_user)
+    secret_code = Board.new(globes_A)
+    user_code = Board.new(globes_B)
+    turn = Turn.new(secret_code, user_code)
 
     assert_instance_of Turn, turn
   end
@@ -26,12 +26,30 @@ class TurnTest < Minitest::Test
     globe_4 = Globe.new('y')
     globes_A = [globe_1, globe_2, globe_3, globe_4]
     globes_B = [globe_4, globe_3, globe_2, globe_1]
-    board_comp = Board.new(globes_A)
-    board_user = Board.new(globes_B)
-    turn = Turn.new(board_comp, board_user)
+    secret_code = Board.new(globes_A)
+    user_code = Board.new(globes_B)
+    turn = Turn.new(secret_code, user_code)
 
-    assert_equal board_comp, turn.board_comp
-    assert_equal board_user, turn.board_user
+    assert_equal secret_code, turn.secret_code
+    assert_equal user_code, turn.user_code
   end
   
+  # def test_it_can_return_codes
+
+  # end
+
+  def test_has_won_is_false_by_default
+    globe_1 = Globe.new('b')
+    globe_2 = Globe.new('g')
+    globe_3 = Globe.new('r')
+    globe_4 = Globe.new('y')
+    globes_A = [globe_1, globe_2, globe_3, globe_4]
+    globes_B = [globe_4, globe_3, globe_2, globe_1]
+    secret_code = Board.new(globes_A)
+    user_code = Board.new(globes_B)
+    turn = Turn.new(secret_code, user_code)
+    
+    # require 'pry'; binding.pry
+    assert_equal false, turn.has_won?
+  end
 end
