1fa5056cf199e721a9a8ea96a794901305cbe7ee
Andrew Smith <andrewpatrick138@gmail.com>
2021-02-11 19:29:06 -0800

    add runner file and begin game logic

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/_'
-class GameTest < Minitest::Test
+require './lib/game'
+class GameTest < Minitest::Testpry
   def test_it_exists
   end
   def test_it_has_attributes
